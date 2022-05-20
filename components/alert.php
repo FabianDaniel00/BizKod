91ea1cb647d38cc476f309c18a700ecab1497846
FabianDaniel00 <danielfabian090@gmail.com>
2022-05-20 13:13:03 +0200

    style fixes

--- components/alert.php
+++ components/alert.php
@@ -4,9 +4,11 @@
 ?>
 
   <div class="msg d-none">
-    <div class="alert alert-<?php echo $alert["type"]; ?>">
+    <div class="alert alert-<?php echo $alert["type"]; ?> pe-5 position-sticky top-1 shadow-sm">
       <?php echo $alert["content"]; ?>
-      <button><i class="fa-solid fa-xmark"></i></button>
+      <button class="position-absolute top-50 translate-middle text-<?php echo $alert["type"]; ?>">
+        <i class="fa-solid fa-xmark"></i>
+      </button>
     </div>
   </div>
 
--- components/footer.php
+++ components/footer.php
@@ -1,4 +1,4 @@
-<footer class="pt-3">
+<footer class="pt-3 position-absolute bottom-0 w-100">
   <ul class="nav justify-content-center border-bottom pb-3 mb-3">
     <li class="nav-item"><a href="<?php echo $from_admin ? "../app/" : ""; ?>home.php" class="nav-link px-2 text-muted">Home</a></li>
   </ul>
