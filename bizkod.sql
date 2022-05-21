9863d8055f26d8968950217856aea00f01ca68e4
Boris2000k <2000boris@gmail.com>
2022-05-21 17:06:41 +0200

    LATLEN fix

--- bizkod.sql
+++ bizkod.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 21, 2022 at 04:47 PM
+-- Generation Time: May 21, 2022 at 05:04 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.0.19
 
@@ -43,8 +43,8 @@ CREATE TABLE `chat` (
 CREATE TABLE `location` (
   `id` int(11) NOT NULL,
   `name` varchar(1024) NOT NULL,
-  `lat` float NOT NULL,
-  `lon` float NOT NULL,
+  `lat` decimal(64,10) NOT NULL,
+  `lon` decimal(64,10) NOT NULL,
   `picture` varchar(1024) NOT NULL,
   `description` varchar(2048) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -54,17 +54,17 @@ CREATE TABLE `location` (
 --
 
 INSERT INTO `location` (`id`, `name`, `lat`, `lon`, `picture`, `description`) VALUES
-(1, 'Trg Žrtava Fašizma', 46.0992, 19.6591, 'hosok.jpg', 'Monument to fallen soldiers and victims of fascism, work of the sculptor Toma Rosandić, was ceremoniously opened on October 30, 1952.'),
-(3, 'National Theatre', 46.1008, 19.67, 'szinhaz.jpg', 'Built in 1854 as the first monumental public building in Subotica, was razed with the purpose of reconstruction by City authorities in 2007, although it was declared a historic monument under state protection in 1983.'),
-(4, 'Lenin Park', 46.1016, 19.6698, 'lenin.jpg', 'Park found in the heart of Subotica.'),
-(5, 'Gradski Stadion FK Spartak', 46.0817, 19.6765, 'spartak.jpg', 'Subotica City Stadium is a multi-purpose stadium located in Subotica, Serbia. With a capacity of 13,000 people, it is currently used mostly for football matches and is the home ground of FK Spartak Subotica from 1945.'),
-(6, 'Church of Holy Greatmartyr Demetrius of Thessaloniki', 46.0725, 19.6797, 'sandor_templom.jpg', 'The Serbian Orthodox Church of St. Dimitri is located in Aleksandrovo, a suburb of the City of Subotica, erected in 1818 and has the status of a cultural monument of great importance.'),
-(7, 'StopShop ', 46.1006, 19.6985, 'shopi.jpg', 'STOP SHOP Subotica is located in Vojvodina, in the very north  of Serbia. It offers a variety of brands as well as a restaurant so everyone can find exactly what they need.'),
-(8, 'Roda Mega', 46.1007, 19.7033, 'rodic.jpg', 'Retail store for household tools and everyday needs.'),
-(9, 'Lake Palić', 46.0885, 19.7575, 'palic.jpg', 'Lake Palić is a lake 8 kilometres from Subotica, near the town of Palić, in Serbia. It covers an area of 3.8 square kilometres.'),
-(10, 'Hala Sportova', 46.1096, 19.6552, 'sportcsarnok.jpg', 'The sports hall is a multifunctional facility that has a gymnasium, a small hall, a hall for martial arts, a gym, a large hall intended primarily for athletes, and in addition it is also suitable for organizing events of various types.'),
-(11, 'Dudova Šuma', 46.1104, 19.653, 'dudova.jpg', 'Dudova Šuma is a small forest, located next to Hala Sportova. It contains a running track, spanning exactly 1 kilometer.'),
-(12, 'St. Theresa of Avila Cathedral', 46.0984, 19.6588, 'terezija.jpg', 'The Cathedral of St. Theresa of Avila is a Roman Catholic cathedral and minor basilica located in Subotica, Serbia, the seat of the Diocese of Subotica. It is dedicated to Saint Theresa of Avila.');
+(1, 'Trg Žrtava Fašizma', '46.0991890000', '19.6591330000', 'hosok.jpg', 'Monument to fallen soldiers and victims of fascism, work of the sculptor Toma Rosandić, was ceremoniously opened on October 30, 1952.'),
+(3, 'National Theatre', '46.1008340000', '19.6700070000', 'szinhaz.jpg', 'Built in 1854 as the first monumental public building in Subotica, was razed with the purpose of reconstruction by City authorities in 2007, although it was declared a historic monument under state protection in 1983.'),
+(4, 'Lenin Park', '46.1016150000', '19.6698140000', 'lenin.jpg', 'Park found in the heart of Subotica.'),
+(5, 'Gradski Stadion FK Spartak', '46.0816550000', '19.6765350000', 'spartak.jpg', 'Subotica City Stadium is a multi-purpose stadium located in Subotica, Serbia. With a capacity of 13,000 people, it is currently used mostly for football matches and is the home ground of FK Spartak Subotica from 1945.'),
+(6, 'Church of Holy Greatmartyr Demetrius of Thessaloniki', '46.0725200000', '19.6797000000', 'sandor_templom.jpg', 'The Serbian Orthodox Church of St. Dimitri is located in Aleksandrovo, a suburb of the City of Subotica, erected in 1818 and has the status of a cultural monument of great importance.'),
+(7, 'StopShop ', '46.1006140000', '19.6985080000', 'shopi.jpg', 'STOP SHOP Subotica is located in Vojvodina, in the very north  of Serbia. It offers a variety of brands as well as a restaurant so everyone can find exactly what they need.'),
+(8, 'Roda Mega', '46.1007030000', '19.7033140000', 'rodic.jpg', 'Retail store for household tools and everyday needs.'),
+(9, 'Lake Palić', '46.0884720000', '19.7575380000', 'palic.jpg', 'Lake Palić is a lake 8 kilometres from Subotica, near the town of Palić, in Serbia. It covers an area of 3.8 square kilometres.'),
+(10, 'Hala Sportova', '46.1095810000', '19.6551530000', 'sportcsarnok.jpg', 'The sports hall is a multifunctional facility that has a gymnasium, a small hall, a hall for martial arts, a gym, a large hall intended primarily for athletes, and in addition it is also suitable for organizing events of various types.'),
+(11, 'Dudova Šuma', '46.1104030000', '19.6529960000', 'dudova.jpg', 'Dudova Šuma is a small forest, located next to Hala Sportova. It contains a running track, spanning exactly 1 kilometer.'),
+(12, 'St. Theresa of Avila Cathedral', '46.0983860000', '19.6587790000', 'terezija.jpg', 'The Cathedral of St. Theresa of Avila is a Roman Catholic cathedral and minor basilica located in Subotica, Serbia, the seat of the Diocese of Subotica. It is dedicated to Saint Theresa of Avila.');
 
 -- --------------------------------------------------------
 
