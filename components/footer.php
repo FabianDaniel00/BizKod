a4ed924a9950637dfbe7b2811a1082e73b3a9314
Boris2000k <2000boris@gmail.com>
2022-05-22 12:30:02 +0200

    footer

--- components/footer.php
+++ components/footer.php
@@ -1,6 +1,7 @@
 <footer class="pt-3 position-absolute bottom-0 w-100">
   <ul class="nav justify-content-center border-bottom pb-3 mb-3">
     <li class="nav-item"><a href="<?php echo $from_admin ? "../app/" : ""; ?>home.php" class="nav-link px-2 text-muted">Home</a></li>
+    <li class="nav-item"><a href="<?php echo $from_admin ? "../app/" : ""; ?>chat.php" class="nav-link px-2 text-muted">Chat with others</a></li>
   </ul>
   <p class="text-center text-muted">© 2022 BizKod - Team ඞSUS</p>
 </footer>
